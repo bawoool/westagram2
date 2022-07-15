25e89f226b9f0dcd33d08d9ae90a4dc7dbe8ab90
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-15 16:04:07 +0900

    모델링

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from re import M
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model): 
+    name       = models.CharField(max_length=40)
+    email      = models.CharField(max_length=100, unique=True)
+    password   = models.CharField(max_length=400)
+    phone      = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        db_table = 'users'
